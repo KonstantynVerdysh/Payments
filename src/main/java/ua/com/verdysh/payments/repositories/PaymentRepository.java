31c7f3b71d527991854bbf799ce21d3542ebfd52
Verdysh <verdysh1991@gmail.com>
2021-02-12 18:15:53 +0200

    Update repository

--- src/main/java/ua/com/verdysh/payments/repositories/PaymentRepository.java
+++ src/main/java/ua/com/verdysh/payments/repositories/PaymentRepository.java
@@ -3,13 +3,14 @@ package ua.com.verdysh.payments.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import ua.com.verdysh.payments.domain.Account;
 import ua.com.verdysh.payments.domain.Payment;
 
 import java.util.Collection;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-    @Query("select pay from Payment pay where pay.payer.accountId = :x and pay.recipient.accountId = :y order by pay.timestamp desc")
-    public Collection<Payment> getPaymentsByPayerAndRecipient(@Param("x") Long payerId, @Param("y") Long recipientId);
+    @Query(value = "select pay from Payment pay where pay.payer = :x and pay.recipient = :y order by pay.timestamp desc")
+    Collection<Payment> getPaymentsByPayerAndRecipient(@Param("x") Account payerId, @Param("y") Account recipientId);
 
 }
